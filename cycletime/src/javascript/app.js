6fa790cb6a750fd854c325b6fccf375a1325ef1d
John Martin <curmudgeon+git@rallydev.com>
2014-11-23 18:02:49 -0800

    make chart

--- cycletime/src/javascript/app.js
+++ cycletime/src/javascript/app.js
@@ -6,7 +6,7 @@ Ext.define('CustomApp', {
     group_field: 'State',
     items: [
         {xtype:'container',itemId:'selector_box', layout: { type:'hbox' }, defaults: { margin: 5 }},
-        {xtype:'container',itemId:'display_box'},
+        {xtype:'container',itemId:'display_box', margin: 10},
         {xtype:'tsinfolink'}
     ],
     launch: function() {
@@ -51,40 +51,70 @@ Ext.define('CustomApp', {
             xtype:'rallybutton',
             text:'Calculate...',
             disabled: true,
-            itemId: 'calculate_button'
+            itemId: 'calculate_button',
+            listeners: {
+                scope:this,
+                click:this._makeChart
+            }
         });
     },
     _enableButtonIfNecessary: function() {
         this.logger.log("Start/End",this.start_state, this.end_state);
         this.down('#calculate_button').setDisabled(true);
 
-
         if ( this.end_state && this.start_state ) {
             var start_store = this.start_state.store;
             var end_store = this.end_state.store;
-            this.logger.log(start_store.indexOf(this.start_state), end_store.indexOf(this.end_state));
+            this.logger.log("Indices of start/stop", start_store.indexOf(this.start_state), end_store.indexOf(this.end_state));
             if (start_store.indexOf(this.start_state) < end_store.indexOf(this.end_state)) {
                 this.down('#calculate_button').setDisabled(false);
             }
         }
     },
-    _loadAStoreWithAPromise: function(model_name, model_fields){
-        var deferred = Ext.create('Deft.Deferred');
+    _makeChart: function(button){
+        this.down('#display_box').removeAll();
         
-        var defectStore = Ext.create('Rally.data.wsapi.Store', {
-            model: model_name,
-            fetch: model_fields,
-            autoLoad: true,
-            listeners: {
-                load: function(store, records, successful) {
-                    if (successful){
-                        deferred.resolve(store);
-                    } else {
-                        deferred.reject('Failed to load store for model [' + model_name + '] and fields [' + model_fields.join(',') + ']');
+        var project_oid = this.getContext().getProject().ObjectID;
+        this.down('#display_box').add({
+            xtype:'rallychart',
+            storeConfig: {
+                find: {
+                    '_TypeHierarchy': this.model
+                },
+                fetch: ['ObjectID', this.group_field, '_ValidFrom', '_PreviousValues'],
+                hydrate: [this.group_field],
+                sort: { '_ValidFrom': 1 }
+            },
+            calculatorType: 'CycleCalculator',
+            calculatorConfig: {
+                initial_state: this.start_state.get('name'),
+                final_state: this.end_state.get('name'),
+                group_field: this.group_field
+            },
+            chartConfig: {
+                chart: {
+                    type: 'column'
+                },
+                title: {
+                    text: 'Cycle Time'
+                },
+                
+                xAxis: {
+                    tickmarkPlacement: 'on',
+                    tickInterval: 30,
+                    title: {
+                        text: 'Date Entered Final State'
+                    }
+                },
+                yAxis: [{title:{text: 'Days'}}],
+                plotOptions: {
+                    series: {
+                        marker: { enabled: false },
+                        stacking: 'normal'
                     }
                 }
+                
             }
         });
-        return deferred.promise;
     }
 });
